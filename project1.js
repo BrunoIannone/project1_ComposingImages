8035fc9bf96146b20139155b61db942fefacf707
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-03-13 21:43:01 +0100

    Ready for code review

--- project1.js
+++ project1.js
@@ -4,19 +4,39 @@
 
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 
-function composite(bgImg, fgImg, fgOpac,fgPos) {
-    for (var y = 0  ; y < fgImg.height; y++) {
-        for (var x = 0; x < fgImg.width; x++) {
-            var fgIndex = y * fgImg.width * 4 + x * 4;
-            var bgIndex = y * bgImg.width * 4 + x * 4;
+function composite(bgImg, fgImg, fgOpac, fgPos) {
+    
+    for (var y = fgPos.y; y < fgImg.height + fgPos.y; y++) {
+        for (var x = fgPos.x; x < fgImg.width + fgPos.x; x++) {
+            if (x >= bgImg.width || y >= bgImg.height || (x <= 0 && y >= 0)) {
 
-            var front_scaled_alpha = (fgImg.data[fgIndex + 3] * fgOpac)/255;
+                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4;
 
-            bgImg.data[bgIndex + 0] = front_scaled_alpha * fgImg.data[fgIndex] + (1 - front_scaled_alpha) * bgImg.data[bgIndex];
-            bgImg.data[bgIndex + 1] = front_scaled_alpha * fgImg.data[fgIndex + 1] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 1];
-            bgImg.data[bgIndex + 2] = front_scaled_alpha * fgImg.data[fgIndex + 2] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 2];
-            bgImg.data[bgIndex + 3] = front_scaled_alpha * fgImg.data[fgIndex + 3] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 3];
+                bgImg.data[bgIndex + 0] = bgImg.data[bgIndex];
+                bgImg.data[bgIndex + 1] = bgImg.data[bgIndex + 1];
+                bgImg.data[bgIndex + 2] = bgImg.data[bgIndex + 2];
+                bgImg.data[bgIndex + 3] = bgImg.data[bgIndex + 3];
+                
+                
+
+            }
+            else {
+                
+                var fgIndex = (y - fgPos.y) * (fgImg.width) * 4 + (x - fgPos.x) * 4;
+                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4;
+                var front_scaled_alpha = (fgImg.data[fgIndex + 3] * fgOpac) / 255;
+
+                bgImg.data[bgIndex + 0] = front_scaled_alpha * fgImg.data[fgIndex] + (1 - front_scaled_alpha) * bgImg.data[bgIndex];
+                bgImg.data[bgIndex + 1] = front_scaled_alpha * fgImg.data[fgIndex + 1] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 1];
+                bgImg.data[bgIndex + 2] = front_scaled_alpha * fgImg.data[fgIndex + 2] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 2];
+                bgImg.data[bgIndex + 3] = front_scaled_alpha * fgImg.data[fgIndex + 3] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 3];
+
+
+
+            }
+
         }
     }
+    return
 }
 
