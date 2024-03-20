fa2aa83dc287a535957a00fb4edd89e154a9f986
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-03-20 10:42:39 +0100

    HW delivery

--- project1.js
+++ project1.js
@@ -8,10 +8,12 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
     
     for (var y = fgPos.y; y < fgImg.height + fgPos.y; y++) {
         for (var x = fgPos.x; x < fgImg.width + fgPos.x; x++) {
-            if (x >= bgImg.width || y >= bgImg.height || (x <= 0 && y >= 0)) {
 
-                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4;
+            if (x >= bgImg.width || y >= bgImg.height || (x < 0 && y >= 0)) { // Rough border check for ignoring foreground pixels outside the background image.
+                
+                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4; // computation of background index
 
+                //Keep the background pixels unchanged
                 bgImg.data[bgIndex + 0] = bgImg.data[bgIndex];
                 bgImg.data[bgIndex + 1] = bgImg.data[bgIndex + 1];
                 bgImg.data[bgIndex + 2] = bgImg.data[bgIndex + 2];
@@ -22,10 +24,13 @@ function composite(bgImg, fgImg, fgOpac, fgPos) {
             }
             else {
                 
-                var fgIndex = (y - fgPos.y) * (fgImg.width) * 4 + (x - fgPos.x) * 4;
-                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4;
-                var front_scaled_alpha = (fgImg.data[fgIndex + 3] * fgOpac) / 255;
+                var fgIndex = (y - fgPos.y) * (fgImg.width) * 4 + (x - fgPos.x) * 4; // computation of foreground index 
+                var bgIndex = (y) * (bgImg.width) * 4 + (x) * 4;// computation of background index
+                
+                var front_scaled_alpha = (fgImg.data[fgIndex + 3] * fgOpac) / 255; //Normalization of the alpha value
 
+
+                // Application of Alpha Blending formula 
                 bgImg.data[bgIndex + 0] = front_scaled_alpha * fgImg.data[fgIndex] + (1 - front_scaled_alpha) * bgImg.data[bgIndex];
                 bgImg.data[bgIndex + 1] = front_scaled_alpha * fgImg.data[fgIndex + 1] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 1];
                 bgImg.data[bgIndex + 2] = front_scaled_alpha * fgImg.data[fgIndex + 2] + (1 - front_scaled_alpha) * bgImg.data[bgIndex + 2];
